80ca3f196dcb8ab13dc873be5affb9f2b5fef1f7
harshmathur86 <99mharsh@gmail.com>
2026-01-11 12:47:05 +0530

    logging config changes

--- src/quiztime/mongodb/mongo_utils.py
+++ src/quiztime/mongodb/mongo_utils.py
@@ -1,11 +1,8 @@
-import logging
+
 from pymongo import MongoClient
 import os
 from quiztime.constant import QTConstants
-
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
-)
+from quiztime.utils.logger import log
 
 DB_HOST = os.getenv("DB_HOST")
 DB_CLIENT = os.getenv("DB_CLIENT")
@@ -22,9 +19,9 @@ async def update_chat_id(chat_id):
     try:
         # await asyncio.sleep(3.5)
         if await is_chat_id_present(chat_id) is False:
-            logging.info(f"{chat_id} - MongoDB - New user arrived")
+            log.info(f"{chat_id} - MongoDB - New user arrived")
             users_collection.insert_one({"_id": chat_id})
         else:
-            logging.info(f"{chat_id} - MongoDB - chatID already present existing user")
+            log.info(f"{chat_id} - MongoDB - chatID already present existing user")
     except Exception as e:
-        logging.error(QTConstants.MONGO_DB_ERROR + " Exception occurred while inserting chat ID", e)
+        log.error(QTConstants.MONGO_DB_ERROR + " Exception occurred while inserting chat ID", e)
